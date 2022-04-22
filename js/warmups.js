005b469108e616297b81844d13b323d8219d4a02
Scott Roberts <scott.w.roberts70@gmail.com>
2022-04-22 09:18:29 -0500

    Added FizzBuzz warm-up

--- js/warmups.js
+++ js/warmups.js
@@ -1,4 +1,17 @@
 "use strict";
 
-let warmUp = "Yay! I completed the warm-up!";
-console.log(warmUp);
+// let warmUp = "Yay! I completed the warm-up!";
+// console.log(warmUp);
+
+for (let i = 0; i < 101; i++) {
+    if (i % 15 == 0) {
+        console.log("Fizzbuzz")
+
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else console.log(i);
+}
+
+
