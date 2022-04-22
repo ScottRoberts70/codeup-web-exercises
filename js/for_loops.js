31b1c0ebd4af6bd18df7233e0797e4b8a0e993ba
Scott Roberts <scott.w.roberts70@gmail.com>
2022-04-22 10:47:16 -0500

    Made additonal comments and examples

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,40 +1,47 @@
+"use strict";
+(function(){
+
 // //Exercise #2 FOR LOOP
  function showMultiplicationTable(input){
     for (let i = 1; i < 11; i++) {
         console.log(input + " x " + i + " = " + (input*i));
     }
 }
+// used to call:  showMultiplicationTable(27);
 
 //Exercise #3 FOR LOOP
 for (let i = 1; i < 11; i++){
-    let isRndNum = Math.floor(Math.random()* 21) + 180;
-    if (isRndNum %2 ===0) {
+    let isRndNum = Math.floor(Math.random()* 181) + 20;
+    if (isRndNum % 2 ===0) {
         console.log(isRndNum + " is Even!");
     }else{
         console.log(isRndNum + " is odd!");
     }
 }
 
-// Exercise #4 FOR LOOP
+// Exercise #4 FOR LOOP within a loop
 for(let i = 0; i < 10; i++){
     let txt = '';
 
 for(let j = 0; j < i; j++){
-    txt=txt + i;
+    txt=txt + i;            // can be txt += i;
 }console.log(txt);
 }
 
-//same output but different examples
+//same output but different examples the Michael Special
 for (let i = 1; i < 10; i++){
         let string = i.toString().repeat(i);
-    console.log(string);
+    console.log(string);  // also  console.log(i.toString().repeat(i));
 }
 
 // Exercise #5 FOR LOOP
-for (let i = 100; i > 0; i-=5){
+    // in a function :  function funcName(val){
+    //funcName (100);   (calls function at end)
+for (let i = 100; i > 0; i -=5){
     console.log(i);
     }
 
+})();
 // ToDO Extra from Slack
 
 // todo Create a function that will replace every other letter of a string with a “Z”. (example: replaceLettersWithZ(“javascript”) // returns “jZvZsZrZpZ”;) This can be accomplished with a for loop,
