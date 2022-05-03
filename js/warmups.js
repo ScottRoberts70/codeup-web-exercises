e8630e5a6865635b365bdc341b3b323481e89835
Scott Roberts <scott.w.roberts70@gmail.com>
2022-05-03 09:28:33 -0500

    flex box Exercise, and finished warmup

--- js/warmups.js
+++ js/warmups.js
@@ -65,25 +65,96 @@
 // }
 // console.log(alphaBet("codeup"));
 
-function arryNum([num1, num2, num3, num4]){
-    return num1 + num2 + num3 +num4;
-}
-console.log(arryNum([2, 4, 2, 2]));
+// function arryNum([num1, num2, num3, num4]){
+//     return num1 + num2 + num3 +num4;
+// }
+// console.log(arryNum([2, 4, 2, 2]));
+//
+// function arryName(arr) {
+//     let sum = 0;
+//     for (let el of arr) {
+//         sum += el;
+//     }
+//     return sum;
+// }
+//
+// console.log(arryName([1, 1, 1, 1]));
+//
+// const array = [1, 2, 3, 4];
+// let sum = 0;
+//
+// for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+// }
+// console.log(sum);
 
-function arryName(arr) {
-    let sum = 0;
-    for (let el of arr) {
-        sum += el;
+// Create a function, returnProductEssentialDetails, that takes in a product object and returns a simpler product object containing only the product name and price in cents.
+
+
+//     Examples...
+//
+let product1 = {
+    name: 'Hammar',
+    priceInCents: 400,
+    description: 'It is a a hammar.',
+    inventory: 25034
+}
+
+let product2 = {
+    name: 'Computer',
+    priceInCents: 40000,
+    description: 'It is a computer.',
+    inventory: 33000
+}
+
+let product3 = {
+    name: 'Ruler',
+    priceInCents: 1000,
+    description: 'It is a ruler.',
+    inventory: 2200
+}
+
+function returnProductEssentialDetails(product) {
+    return {
+        name:product.name,
+        priceInCents: product.priceInCents,
     }
-    return sum;
 }
 
-console.log(arryName([1, 1, 1, 1]));
-
-const array = [1, 2, 3, 4];
-let sum = 0;
-
-for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-}
-console.log(sum);
+//
+//     {
+//         name: 'Hammar',
+//         priceInCents: 400
+//     }
+//
+//
+//
+//
+// returnProductEssentialDetails(product2); // returns...
+//
+//
+//
+//     {
+//         name: 'Computer',
+//         priceInCents: 40000
+//     }
+//
+//
+//
+//
+// returnProductEssentialDetails(product3); // returns...
+//
+//
+//
+//     {
+//         name: 'Ruler',
+//         priceInCents: 1000
+//     }
+//
+//
+// //
+//
+//
+// BONUS (if time permits)
+//
+// Create a function, returnAllProductsEssentialDetails, that takes in an array of product objects and returns an array of product objects containing only the essential details of the product name and price in cents.
