b8742cce1c33effb75947f4e450ac3aafb5e761d
Scott Roberts <scott.w.roberts70@gmail.com>
2022-05-19 14:39:46 -0500

    jquery exercises

---
+++ js/jqueryJS.js
@@ -0,0 +1,32 @@
+//Practice during the lecture//
+// $(function(){
+//   let thisId = $("#thisTitle").html();
+//   alert(thisId);
+// });
+
+// $(function(){
+//     let thisDiv1 = $("#thisTitle").html();
+//     alert(thisDiv1);
+// });
+
+// $(function(){
+//     let thisDiv1 = $("#thisDiv, ul, li" ).html();
+//     alert(thisDiv1);
+// });
+//Only Returned the first element in the alert//
+
+// $(".codeup").css('border', '1px solid red');
+
+//removed one element and indeed it doesn't have a border//
+//No border on the id, because the other ws a class. Unless I select multiple id's and classes together.//
+
+// $("li").css("font-size", "20px");
+
+// $('.codeup, .liItem, #paraId').css('background-color', '#FFFF00');
+//
+// $(function(){
+//   let alertHeader = $('h1').html();
+//   alert(alertHeader);
+// })
+
+
