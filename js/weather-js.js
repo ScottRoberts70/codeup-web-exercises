bba94780dfad10d619517e737df539aa57bf9ec4
Scott Roberts <scott.w.roberts70@gmail.com>
2022-05-31 17:54:59 -0500

    added comments to JS, HTML and changed some style

--- js/weather-js.js
+++ js/weather-js.js
@@ -21,39 +21,40 @@ setInterval(() => {
     const day = time.getDay();
     const hour = time.getHours();
 
-    ////set to 12 hr using modulus-also allowed zero in the minutes//
-    const hoursIn12hrFormat = hour >= 13 ? hour % 12 : hour
-    const minutes = time.getMinutes() < 10 ? '0' : '' + time.getMinutes();
-    const ampm = hour >= 12 ? 'PM' : 'AM'
+    //formats the time for the current data---///
+    const hoursIn12hrFormat = time.toLocaleTimeString([], {hour: 'numeric', minute: '2-digit'})
 
     //---------concatenate our data adn return to HTML----//
-    timeEl.innerHTML = hoursIn12hrFormat + ':' + minutes + `  <span id="am-pm">${ampm}</span>`
+    timeEl.innerHTML = hoursIn12hrFormat;
     dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
 }, 1000);
 
-//TODO call geolocation based on Nav, may change this to map location on final version//
 getWeatherData();
 
-function getWeatherData() {
+function getWeatherData(latitude, longitude) {
     navigator.geolocation.getCurrentPosition((success) => {
-        console.log(success);
 
 //---- variable for longitude and latitude coordinates from navigator call//
-        let {latitude, longitude} = success.coords;
-
+        if (latitude === undefined || longitude === undefined) {
+            latitude = success.coords.latitude
+            longitude = success.coords.longitude
+        }
 
         ///API CALL using fetch and promise---- USED 2.5 version 3.0 didn't work---also changed kelvin to imperial//
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${OPEN_WEATHER_KEY}`).then(res => res.json()).then(data => {
 
-            console.log(data);
             showWeatherData(data);
         })
     })
+
 }
 
+//----THE INFO I REQUESTED FROM THE API CALL------//
 function showWeatherData(data) {
     let {feels_like, humidity, sunrise, sunset, uvi} = data.current;
+    console.log(data);
 
+    //-----USED LITERALLY TO PLACE HTML IN JS AND RETURN THE DATA TO HTML FORMAT//
     currentWeatherItemsEl.innerHTML = `<div class="weather-item">
                     <div>Feels Like</div>
                     <div>${feels_like}&#176;</div>
@@ -64,7 +65,8 @@ function showWeatherData(data) {
                 </div>
                 <div class="weather-item">
                     <div>Sun Rise</div>
-                    <!--Needed to use the .moment cdn in my header to get the datetimegroup correct *1000 for time zone.--->
+                 
+                    <!--Needed to use the .moment cdn --->
                     <div>${window.moment(sunrise * 1000).format('h:mm a')}</div>
                 </div>
                 <div class="weather-item">
@@ -76,60 +78,74 @@ function showWeatherData(data) {
                     <div>${uvi}</div>
                 </div>`;
 
+
+    //-----BUCKET-------//
     let otherDayForecast = ''
 
     data.daily.forEach((day, idx) => {
         if (idx == 0) {
+
+            //SETS THE INFORMATION FROM THE API FOR CURRENT INFO//
             currentTempEl.innerHTML = `
             <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
             <div class="other">
                 <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
-                <div class="temp">Day - ${day.temp.day}&#176; F</div>
-                <div class="temp">Night - ${day.temp.night}&#176; F</div>
+                <div class="temp">High - ${day.temp.max}&#176; F</div>
+                <div class="temp">Low - ${day.temp.min}&#176; F</div>
             </div>`
         } else {
+
+            //SETS THE INFORMATION FROM THE API FOR THE 7DAYS or OtherDaily//
             otherDayForecast += `
              <div class="weather-forecast-item">
                 <div class="day">${window.moment(day.dt * 1000).format('ddd')}</div>
                 <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <div class="temp">Day - ${day.temp.day}&#176; F</div>
-                <div class="temp">Night - ${day.temp.night}&#176; F</div>
+                <div class="temp">High - ${day.temp.max}&#176; F</div>
+                <div class="temp">Low - ${day.temp.min}&#176; F</div>
             </div> `
         }
     })
-
+//------PUT THE INFO ONTO THE WEBPAGE----------//
     weatherForecastEl.innerHTML = otherDayForecast;
-
-    mapboxgl.accessToken = MAPBOX_API_KEY;
-    const MAP = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/dark-v10', // style URL
-        center: [-98.4691, 29.603415], // starting position [lng, lat]
-        zoom: 10
-        // starting zoom
-    });
-    ////Maybe add a button to toggle between light and dark mode?//
-
-    const MARKER = new mapboxgl.Marker()
-        .setLngLat([-98.4691, 29.603415])
-        .setDraggable(true)
-        .addTo(MAP);
-
-    reverseGeocode({lat: -98.4691, lng: 29.603415}, MAPBOX_API_KEY).then(function (location) {
-        console.log(location);
-    });
-
-    let LONG_LAT = [];
-
-    MARKER.on("dragend", function () {
-        LONG_LAT = MARKER.getLngLat();
-        document.getElementById("place").innerHTML = LONG_LAT.lng + ", " + LONG_LAT.lat;
-        MAP.flyTo({center: [LONG_LAT.lng, LONG_LAT.lat]});
-        reverseGeocode(LONG_LAT, MAPBOX_API_KEY).then(function (data) {
-            document.getElementById("place").innerHTML = data;
-        });
-    });
 }
+
+//--------INITIAL MAP API CALL TO MAPBOX & CANTER POINT----//
+
+mapboxgl.accessToken = MAPBOX_API_KEY;
+const MAP = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
+    center: [-98.4691, 29.603415], // starting position [lng, lat]
+    zoom: 13 // starting zoom
+});
+
+
+//----CODE COPIED FROM MAPBOX TO LET YOU CHOOSE MAP STYLES---//
+const layerList = document.getElementById('menu');
+const inputs = layerList.getElementsByTagName('input');
+
+for (const input of inputs) {
+    input.onclick = (layer) => {
+        const layerId = layer.target.id;
+        MAP.setStyle('mapbox://styles/mapbox/' + layerId);
+    }
+}
+
+//-----SETS GEO LOCATOR FOR THE SEARCH BOX-----//
+const geocoder = new MapboxGeocoder({
+    accessToken: MAPBOX_API_KEY,
+    mapboxgl: mapboxgl
+});
+
+MAP.addControl(geocoder)
+
+//-----USES SEARCH FIELD RESULTS TO RECENTER LOCATION & GATHERS RESULT/DATA----//
+geocoder.on('result', function (data) {
+    getWeatherData(data.result.center[1], data.result.center[0])
+});
+
+
+
 
 
 
